5e3b49337a656bdeb61f344625d5683b42c22ef3
F54101056 <40814498+phi622968143@users.noreply.github.com>
2024-09-25 15:59:46 +0800

    Update action.py

--- vc-robot/servo/action.py
+++ vc-robot/servo/action.py
@@ -1,33 +1,40 @@
 import time
-import asyncio
 from servo import Servo
 
-rt = Servo(0)
-rb = Servo(1)
-lt = Servo(6)
-lb = Servo(7)
+class Action:
+    def __init__(self):
+        # Initialize servos
+        self.rt = Servo(2)
+        self.rb = Servo(3)
+        self.lt = Servo(0)
+        self.lb = Servo(1)
 
-def action_test():
-	lt.lerp(0, 180, 5 , 0.01)
-def servo_default_action():
-	print("Conducting.....")
-	for i in range(10):
-    		lt.lerp(-15, 0, 1, 0.01)
-    		lt.lerp(0, -15, 1, 0.01)
-		time.sleep(0.1)
-		rt.lerp(0, 30, 2, 0.1)
-		rt.lerp(30, 0, 2, 0.1)
-	for i in range(10):
-    		rt.lerp(30, 0, 2, 0.1)
-    		rt.lerp(0, 30, 2, 0.1)
-		time.sleep(0.1)
-		lt.lerp(0, -15, 1, 0.01)
-		lt.lerp(-15, 0, 1, 0.01)
-def forward_action():
-	for i in range(10):
-		lb.lerp(60, 120, 1, 0.01)
-		lb.lerp(120, 60, 1, 0.01)
-	time.sleep(0.1)
-	for i in range(10):
-		rb.lerp(0, 90, 1, 0.01)
-		rb.lerp(90, 0, 1, 0.01)
+    def action_test(self):
+        self.lt.lerp(0, 180, 5, 0.01)
+
+    def servo_default_action(self):
+        print("Conducting.....")
+        for _ in range(10):
+            self.lt.lerp(-15, 0, 1, 0.01)
+            self.lt.lerp(0, -15, 1, 0.01)
+            time.sleep(0.1)
+            self.rt.lerp(0, 30, 2, 0.1)
+            self.rt.lerp(30, 0, 2, 0.1)
+        
+        for _ in range(10):
+            self.rt.lerp(30, 0, 2, 0.1)
+            self.rt.lerp(0, 30, 2, 0.1)
+            time.sleep(0.1)
+            self.lt.lerp(0, -15, 1, 0.01)
+            self.lt.lerp(-15, 0, 1, 0.01)
+
+    def forward_action(self):
+       
+        for _ in range(10):
+            self.lb.lerp(60, 120, 1, 0.01)
+            self.lb.lerp(120, 60, 1, 0.01)
+        time.sleep(0.1)
+
+        for _ in range(10):
+            self.rb.lerp(0, 90, 1, 0.01)
+            self.rb.lerp(90, 0, 1, 0.01)
